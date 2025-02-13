bf1ac814bd8151aa3eca921868aff2da6aa00e8c
Aditya Rai <adirai2901@gmail.com>
2025-02-13 21:05:46 +0530

    Java

--- Arrays/checking.java
+++ Arrays/checking.java
@@ -1,22 +1,23 @@
 
 public class checking {
-    public static void rec(int arr[]){
-        int f = 0, l = arr.length-1;
-
-        while(f<l){
-            int temp = arr[l];
-            arr[l]= arr[f];
-            arr[f]=temp;
-            f++;
-            l--;
+    public static void bubble(int arr[]){
+        for(int i = 0 ; i<arr.length-1;i++){
+            for(int j = 0 ; j <arr.length-1-i;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp = arr[j+1];
+                    arr[j+1]= arr[j];
+                    arr[j]= temp;
+                }
+            }
         }
-    }    
 
-    public static void main(String args[]){
-        int arr[] = {2,4,6,8,10};
-        rec(arr);
-        for(int i= 0;i<arr.length;i++){
+        for(int i = 0 ; i <arr.length;i++){
             System.out.print(arr[i]+" ");
         }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {5,4,2,3,1};
+        bubble(arr);
     }
 }
---
+++ Sorting/selection_sort.java
@@ -0,0 +1,24 @@
+public class selection_sort {
+    public static void sel_sort(int arr[]){
+        for(int i = 0 ; i<arr.length-1;i++){
+            int min = i;
+            for(int j = i+1;j<arr.length;j++){
+                if(arr[j]<arr[min]){
+                    min = j;
+                }
+            }
+            int temp = arr[min];
+            arr[min] = arr[i];
+            arr[i]= temp;
+        }
+        for(int i = 0 ; i< arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+
+    public static void main(String args[]){
+        int arr[]={5,4,2,3,1};
+        sel_sort(arr);
+    }
+}
+    
