791441928b1deac26431cb6cea650280dc607166
Aditya Rai <adirai2901@gmail.com>
2025-02-13 21:14:26 +0530

    Java

--- Arrays/checking.java
+++ Arrays/checking.java
@@ -1,15 +1,17 @@
 
 public class checking {
     public static void bubble(int arr[]){
-        for(int i = 0 ; i<arr.length-1;i++){
-            for(int j = 0 ; j <arr.length-1-i;j++){
-                if(arr[j]>arr[j+1]){
-                    int temp = arr[j+1];
-                    arr[j+1]= arr[j];
-                    arr[j]= temp;
-                }
+       for(int i = 0 ; i <arr.length-1;i++){
+            int min = i;
+        for(int j = i+1;j < arr.length;j++){
+            if(arr[j]<arr[i]){
+                min = j;
             }
         }
+        int temp = arr[min]; 
+        arr[min] = arr[i];
+        arr[i]= temp;
+       }
 
         for(int i = 0 ; i <arr.length;i++){
             System.out.print(arr[i]+" ");
