f89868b1f7df4f1f117ce56052e1a0d2f245bc12
Aditya Rai <adirai2901@gmail.com>
2025-02-17 21:21:24 +0530

    Java

---
+++ Leetcode/_3_sum.java
@@ -0,0 +1,36 @@
+import java.util.*;
+public class _3_sum {
+
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums); // Step 1: Sort the array
+        List<List<Integer>> result = new ArrayList<>();
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue; // Skip duplicates
+
+            int left = i + 1, right = nums.length - 1;
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (sum == 0) {
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+
+                    // Skip duplicate numbers
+                    while (left < right && nums[left] == nums[left + 1]) left++;
+                    while (left < right && nums[right] == nums[right - 1]) right--;
+
+                    left++;
+                    right--;
+                } else if (sum < 0) {
+                    left++; // Increase sum
+                } else {
+                    right--; // Decrease sum
+                }
+            }
+        }
+        return result;
+    }
+}
+
+}
