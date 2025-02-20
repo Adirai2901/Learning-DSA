7e1263efdf91c2e15b6aa39ab708529d75d23451
Aditya Rai <adirai2901@gmail.com>
2025-02-20 20:44:49 +0530

    Java

---
+++ Leetcode/_268_Missing_Number.java
@@ -0,0 +1,14 @@
+public class _268_Missing_Number {
+    class Solution {
+        public int missingNumber(int[] nums) {
+            int n = nums.length;
+            int expectedSum = n * (n + 1) / 2;
+            int actualSum = 0;
+            for (int num : nums) {
+                actualSum += num;
+            }
+            return expectedSum - actualSum;
+        }
+    }
+    
+}
