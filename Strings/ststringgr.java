8386ce2f49afbd954581332b26620095fe6c5f08
Aditya Rai <adirai2901@gmail.com>
2025-03-03 18:57:42 +0530

    Java

---
+++ Strings/ststringgr.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class ststringgr {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    String strr;
+    
+    strr = sc.nextLine();
+    System.out.println(strr);
+    sc.close();
+    }
+    
+}
