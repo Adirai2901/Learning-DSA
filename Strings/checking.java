0a4661c792539aa1294db47f699ea3c4fe23c338
Aditya Rai <adirai2901@gmail.com>
2025-03-06 17:20:47 +0530

    Java

--- Strings/checking.java
+++ Strings/checking.java
@@ -1,26 +1,13 @@
 public class checking {
 
-    public static String toup(String str){
-        StringBuilder sb = new StringBuilder("");
-        char ch = Character.toUpperCase(str.charAt(0));
-        sb.append(ch);
-
-        for(int i = 1; i < str.length();i++){
-            if(str.charAt(i)== ' ' && i < str.length()-1){
-                sb.append(str.charAt(i));
-                i++;
-                sb.append(Character.toUpperCase(str.charAt(i)));
-            }
-            else{
-                sb.append(str.charAt(i));
-            }
-        }
-        return sb.toString();
-    }
-    
     public static void main(String[] args) {
-        String str = "hello frand cahii pii lo";
-
-        System.out.println(toup(str));
+        String str[]={"Apple","Aall","Aat"};
+        if(str[0].charAt(0) == str[1].charAt(0) && str[0].charAt(0) == str[2].charAt(0)){
+            System.out.println("true");
+        }else{
+            System.out.println("mc");
+        }
+       
+       
     }
 }
