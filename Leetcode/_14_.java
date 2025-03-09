4aef1ae0c4978333c961e0f0b37cfeefdfec18b2
Aditya Rai <adirai2901@gmail.com>
2025-03-09 12:18:56 +0530

    Java

---
+++ Leetcode/_14_.java
@@ -0,0 +1,22 @@
+public class _14_ {
+    class Solution {
+        public String longestCommonPrefix(String[] strs) {
+            if (strs == null || strs.length == 0) return "";
+     
+            StringBuilder sb = new StringBuilder();
+            
+            for (int j = 0; j < strs[0].length(); j++) {
+                char ch = strs[0].charAt(j);
+                
+                for (int i = 1; i < strs.length; i++) {
+                    if (j >= strs[i].length() || strs[i].charAt(j) != ch) {
+                        return sb.toString();
+                    }
+                }
+                sb.append(ch);///jhat kuch samjh aya h 
+            }
+            return sb.toString();
+        }
+     }
+     
+}
