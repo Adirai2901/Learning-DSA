934122c97cf4bc2f4f2978e44d867dd7782802c5
Aditya Rai <adirai2901@gmail.com>
2025-03-25 22:53:14 +0530

    JAVA

---
+++ JAVA BASIC/Algorithms/Quick_sort.java
@@ -0,0 +1,10 @@
+public class Quick_sort {
+    public static void printarr(int[] arr){
+        for(int i = 0 ; i < arr.length;i++){
+            System.out.println(arr[i]);
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr = {5,4,3,2,1,6,5};
+    }
+}
