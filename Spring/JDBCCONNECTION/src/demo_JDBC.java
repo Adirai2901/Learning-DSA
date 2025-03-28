ec0d5b753fb1e13b62addc3eb86b9a6e49bbf051
Aditya Rai <adirai2901@gmail.com>
2025-03-29 02:38:50 +0530

    JAVA

--- Spring/JDBCCONNECTION/src/demo_JDBC.java
+++ Spring/JDBCCONNECTION/src/demo_JDBC.java
@@ -6,17 +6,28 @@ public class demo_JDBC {
         String url = "jdbc:postgresql://localhost:5432/Demo";
         String uname = "postgres";
         String pass = "****";
-        String sql = "select * from Student ;";
+//        String sql = "insert into student values (5,'Monkey',99)";
 
         Connection con = DriverManager.getConnection(url,uname,pass);
         System.out.println("Connect established");
 
         Statement st = con.createStatement();
 
-        ResultSet rs = st.executeQuery(sql);
-        //rs.next();
+//        ResultSet rs = st.executeQuery(sql);
+//        rs.next();
 //      String name = rs.getString("Name");
 //      System.out.println(name);
+//        while(rs.next()){
+//            System.out.print(rs.getInt(1)+"-");
+//            System.out.print(rs.getString(2)+"-");
+//            System.out.println(rs.getInt(3));
+//        }
+        boolean x = st.execute("insert into student values (6,'cow',99)");
+//        System.out.println(x);
+
+        
+
+        ResultSet rs = st.executeQuery("Select * from student");
         while(rs.next()){
             System.out.print(rs.getInt(1)+"-");
             System.out.print(rs.getString(2)+"-");
