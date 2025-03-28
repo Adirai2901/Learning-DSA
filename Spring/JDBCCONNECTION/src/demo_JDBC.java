eaa01d797d02872e3fa509ad96a2d1f3cac11648
Aditya Rai <adirai2901@gmail.com>
2025-03-29 02:27:37 +0530

    JAVA

--- Spring/JDBCCONNECTION/src/demo_JDBC.java
+++ Spring/JDBCCONNECTION/src/demo_JDBC.java
@@ -14,10 +14,19 @@ public class demo_JDBC {
         Statement st = con.createStatement();
 
         ResultSet rs = st.executeQuery(sql);
-        rs.next();
-        String name = rs.getString("Name");
-        System.out.println(name);
+        //rs.next();
+//      String name = rs.getString("Name");
+//      System.out.println(name);
+        while(rs.next()){
+            System.out.print(rs.getInt(1)+"-");
+            System.out.print(rs.getString(2)+"-");
+            System.out.println(rs.getInt(3));
+        }
+
+
         con.close();
+
+
         System.out.println("connection closed");
 
     }
